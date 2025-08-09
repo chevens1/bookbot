65cb1fc47b99bf18b2c8786bb6d4476f2f80c0c8
chevens1 <chevens@hotmail.com>
2025-08-09 12:01:05 +0100

    final code version

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 # main.py
 from stats import count_words, letter_counts, sort_counts_desc
+import sys
 
 def get_book_text(filepath):
     with open(filepath, encoding="utf-8") as f:
         return f.read()
 
 def main():
-    path = "books/frankenstein.txt"
-    text = get_book_text(path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)  # Exit with status code 1 to indicate incorrect usage
 
+    path = sys.argv[1]  # Use the CLI argument instead of a hardcoded path
+    text = get_book_text(path)
     # Compute stats
     total_words = count_words(text)
     counts_sorted = sort_counts_desc(letter_counts(text))
